6a95eb6ab9a3c35e5deeb6cc9a74527fb5c667ce
SimLukos <simonas.lukosevicius92@gmail.com>
2022-06-15 20:14:48 +0300

    js pirma uzduotis

---
+++ JS/1paskaita/index.js
@@ -0,0 +1,23 @@
+// const myAge = 18;
+// if (myAge >= 29) {
+//   console.log("of legal age");
+// } else {
+//   console.log("you are too young");
+// }
+
+let num = 29;
+let str = "29";
+
+// console.log(num == str);
+// console.log(num === str);
+
+const two = num == str;
+const three = num === str;
+
+two ? console.log("teisingai") : console.log("neteisingai");
+
+three ? console.log("teisingai") : console.log("neteisingai");
+
+0 ? console.log("teisingai") : console.log("neteisingai"); //nulis kaip false
+
+1 ? console.log("teisingai") : console.log("neteisingai"); //vienas kaip true
