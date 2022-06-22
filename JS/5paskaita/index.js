de9c67da9a4ec42ed824adad4bfadd3b174fcbfd
SimLukos <simonas.lukosevicius92@gmail.com>
2022-06-22 20:32:24 +0300

    javascript keiciam html atributus

---
+++ JS/5paskaita/index.js
@@ -0,0 +1,30 @@
+const sudetis = function (one, two) {
+  const result = one + two;
+  return result;
+};
+
+const atimtis = function (one, two) {
+  const result = one - two;
+  return result;
+};
+
+const dalyba = function (one, two) {
+  const result = one / two;
+  return result;
+};
+
+const daugyba = function (one, two) {
+  const result = one * two;
+  return result;
+};
+
+const liekana = function (one, two) {
+  const result = one % two;
+  return result;
+};
+
+console.log(sudetis(29, 1));
+console.log(atimtis(29, 1));
+console.log(dalyba(29, 1));
+console.log(daugyba(29, 1));
+console.log(liekana(29, 1));
